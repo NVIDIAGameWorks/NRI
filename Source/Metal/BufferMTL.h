5d5d8f33f47115720f874a7851534d77fefcd45c
Michael Pollind <mpollind@gmail.com>
2024-08-29 00:01:17 -0700

    feat: add templte classes
    Signed-off-by: Michael Pollind <mpollind@gmail.com>

---
+++ Source/Metal/BufferMTL.h
@@ -0,0 +1,48 @@
+
+
+#import <MetalKit/MetalKit.h>
+
+struct DeviceMTL;
+struct MemoryMTL;
+
+struct BufferMTL {
+
+    inline BufferMTL(DeviceMTL& device)
+        : m_Device(device) {
+    }
+
+    inline id<MTLBuffer> GetHandle() const {
+        return pBuffer;
+    }
+
+    inline VkDeviceAddress GetDeviceAddress() const {
+        return m_DeviceAddress;
+    }
+
+    inline DeviceMTL& GetDevice() const {
+        return m_Device;
+    }
+
+    inline const BufferDesc& GetDesc() const {
+        return m_Desc;
+    }
+
+    ~BufferMTL();
+    
+    Result Create(const BufferDesc& bufferDesc);
+    Result Create(const BufferVKDesc& bufferDesc);
+    Result Create(const AllocateBufferDesc& bufferDesc);
+
+private:
+    DeviceVK& m_Device;
+    id<MTLBuffer> pBuffer;
+    uint8_t* m_MappedMemory = nullptr;
+    uint64_t m_MappedMemoryOffset = 0;
+    uint64_t m_MappedMemoryRangeSize = 0;
+    uint64_t m_MappedMemoryRangeOffset = 0;
+    BufferDesc m_Desc = {};
+    bool m_OwnsNativeObjects = true;
+};
+
+
+
---
+++ Source/Metal/TextureMTL.h
@@ -0,0 +1,21 @@
+// © 2021 NVIDIA Corporation
+
+#pragma once
+
+
+namespace nri {
+
+struct DeviceMTL;
+
+struct TextureMTL {
+
+    inline TextureMTL(DeviceMTL& device)
+        : m_Device(device) {
+    }
+private:
+    DeviceMTL& m_Device;
+
+};
+
+}
+
