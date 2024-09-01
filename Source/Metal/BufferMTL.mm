3ef604ea932d8d7c9177589d5e3950c2785f12e6
Michael Pollind <mpollind@gmail.com>
2024-09-01 00:01:47 -0700

    feat: add more to api
    Signed-off-by: Michael Pollind <mpollind@gmail.com>

--- Source/Metal/BufferMTL.mm
+++ Source/Metal/BufferMTL.mm
@@ -1,2 +1,13 @@
 #include "BufferMetal.h"
 
+Result BufferMTL::Create(const BufferDesc& bufferDesc) {
+    return RESULT::SUCCESS;
+}
+
+Result BufferMTL::Create(const BufferVKDesc& bufferDesc) {
+    return RESULT::SUCCESS;
+}
+
+Result BufferMTL::Create(const AllocateBufferDesc& bufferDesc) {
+    return RESULT::SUCCESS;
+}
