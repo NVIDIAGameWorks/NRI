a84371e369ab12adeb920d6615e4ecea58b85427
Michael Pollind <mpollind@gmail.com>
2024-11-30 21:11:41 -0800

    feat: add descriptor binding logic
    Signed-off-by: Michael Pollind <mpollind@gmail.com>

--- Source/Metal/DescriptorPoolMTL.h
+++ Source/Metal/DescriptorPoolMTL.h
@@ -15,31 +15,29 @@ struct DescriptorPoolMTL {
         , m_AllocatedSets(device.GetStdAllocator()) {
         m_AllocatedSets.reserve(64);
     }
-//
-//    inline operator VkDescriptorPool() const {
-//        return m_Handle;
-//    }
-//
+
     inline DeviceMTL& GetDevice() const {
         return m_Device;
     }
 
     ~DescriptorPoolMTL();
-
     Result Create(const DescriptorPoolDesc& descriptorPoolDesc);
 
     //================================================================================================================
     // NRI
     //================================================================================================================
-
+    
+//    size_t GetNumberOfArugmentsAlloc();
     void SetDebugName(const char* name);
     void Reset();
     Result AllocateDescriptorSets(const PipelineLayout& pipelineLayout, uint32_t setIndex, DescriptorSet** descriptorSets, uint32_t instanceNum, uint32_t variableDescriptorNum);
 
 private:
     DeviceMTL& m_Device;
+    size_t m_ArgumentOffset = 0;
     Vector<DescriptorSetMTL*> m_AllocatedSets;
-    //VkDescriptorPool m_Handle = VK_NULL_HANDLE;
+    id<MTLBuffer> m_ArgumentBuffer;
+    
     uint32_t m_UsedSets = 0;
     bool m_OwnsNativeObjects = true;
 };
